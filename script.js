c8b7980689ec97f4e9420a89ba76f83d1fdce278
uwnrg <sahibjot@live.ca>
2015-02-15 00:31:47 -0500

    Created landing page with name

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+/*
+    x = 1;
+    (function myLoop (i) {
+       setTimeout(function () {
+
+          $("#pic" + x).css("")
+
+
+          x++;
+          if (--i) myLoop(i);
+       }, 3000)
+   })(4);*/
+});
