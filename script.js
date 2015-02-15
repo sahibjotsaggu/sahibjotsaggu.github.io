c4dc02934e4b0c951d14b697a2a6da0b53cceb37
uwnrg <sahibjot@live.ca>
2015-02-15 00:34:28 -0500

    Deleted unnecessary images

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-/*
-    x = 1;
-    (function myLoop (i) {
-       setTimeout(function () {
 
-          $("#pic" + x).css("")
-
-
-          x++;
-          if (--i) myLoop(i);
-       }, 3000)
-   })(4);*/
 });
