082ced21d520006b8c6b6b3a9a5be108b2a4f60c
uwnrg <sahibjot@live.ca>
2015-02-15 02:36:56 -0500

    made nav page more translucent

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         if ($(".nav_page").css("display") == "none") {
             $(".nav_button").css("background", "rgba(0,0,0,0.9)");
             $(".nav_page").fadeIn();
+            $(".name").fadeOut();
             $(".nav_button div#middle").css("opacity", "0");
             $(".nav_button div#top").css("top", "25px");
             $(".nav_button div#top").css("transform", "rotateZ(-45deg)");
