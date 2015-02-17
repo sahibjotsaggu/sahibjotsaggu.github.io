b83c776482e70d14cd3951d62691c97ed78016ba
uwnrg <sahibjot@live.ca>
2015-02-16 19:36:17 -0500

    cleaned up landing page and started first tile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".down_arrow").click(function() {
         $("html, body").animate({
-            scrollTop : $(".tile2").offset().top
+            scrollTop : $(".tile2_wrapper").offset().top
         }, "easeOutBounce");
         return false;
     });
