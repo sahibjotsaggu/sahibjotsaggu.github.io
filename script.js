6dc9188f97b3288418b3c98442e6869308ee5d88
uwnrg <sahibjot@live.ca>
2015-03-02 10:15:12 -0500

    changed bg img to .jpg from .png to make it load faster

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    /*$("footer").css("top", $(window).height() + $(".tile2_wrapper #greeting").height() + $(".tile2_wrapper #about_me").height() + $(".projects_wrapper").height() + "px");*/
     $(".tile2_wrapper .projects_wrapper #projects_list div").css("height", $("#projects_list").width()*(9/16));
 	var get_col_width = $(".projects_wrapper .projects_list ul li").width();
     $(".projects_wrapper .projects_list ul li").css("height", get_col_width);
