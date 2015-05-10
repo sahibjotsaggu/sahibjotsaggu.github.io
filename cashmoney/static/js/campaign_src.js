9e3b1347fd837d9a0c0fc2abe8fdc34f1b8ccdb9
sahibjotsaggu <sahibjot@live.ca>
2015-05-10 13:02:41 -0400

    mostly done the campaign creation frontend

--- cashmoney/static/js/campaign_src.js
+++ cashmoney/static/js/campaign_src.js
@@ -1,10 +1,38 @@
+/*
+Things to do:
+- when the user comes back from the campaign preview page, input filled values.
+- do error checking
+*/
+
 $(document).ready(function() {
+
+	/*
+	var all_sess_vars = ["user_camp_title", "user_camp_funding",
+	"user_camp_image", "user_camp_tagline", "user_camp_description",
+	"user_camp_duration"];
+	var sess_counter = 0;
+	for (var x = 0; x < all_sess_vars.length; x++) {
+		alert(document.getElementById(all_sess_vars[x]).value);
+	}
+	/* checking if everything has been filled out */
+	/*for (var x = 0; x < all_sess_vars.length; x++) {
+		if (all_sess_vars[x] != undefined) {
+			all_sess_vars[x] = 1;
+
+		}
+		else {
+			all_sess_vars[x] = 0;
+		}
+	}*/
+
+	//for (var y = 0; y < all_sess_vars.length)
+
 	$(".camp_txtInp").change(function() {
 		/* alert($(this).attr('id')); << USE THIS FOR ERROR CHECKING!! */
 		if (document.getElementById($(this).attr('id')).value.length > 0) {
-			$(this).css("background", "#2B303B");
 			$(this).css("border-bottom", "2px solid #2B303B");
-			$(this).css("color", "#FFF");
+			
+
 		}
 		else {
 			$(this).css("background", "#FFF");
@@ -52,16 +80,16 @@ $(document).ready(function() {
 				alert("Sorry, your browser does not support HTML5 localStorage.");
 			}
 		}		
-
 	});
 
 	$("#fileupload").change(function(){
 		readURL(this);
 		$("#pic_input_div").css("background-size", "cover");
-		$("#pic_input_div").css("border", "1px solid #2B303B");
+		$("#pic_input_div").css("border", "2px solid #2B303B");
 		$(".camp_inp_pic_wrapper").css("background", "url('static/img/checkmark.png') no-repeat center center");
 	});
 
+	/* camp duration */
 	$(".camp_dur .camp_dur_list ul li").click(function() {
 		var click_id = $(this).attr('id');
 		$(".camp_dur .camp_dur_list ul li").css("background", "#FFF");
@@ -78,6 +106,7 @@ $(document).ready(function() {
 		}
 	});
 
+	/* camp description */
 	$("#camp_desc").change(function() {
 		var txtarea_val = document.getElementById("camp_desc").value;
 		if (txtarea_val.length > 0) {
@@ -95,11 +124,13 @@ $(document).ready(function() {
 	});
 
 	$(".camp_preview").click(function() {
-		window.location = "test.html";
+		/* error checking here */
+		window.location = "campaign_preview.html";
 	});
 
 });
 
+/* getting the url of the img input and setting the img on the appropriate bg */
 function readURL(input) {
 	if (input.files && input.files[0]) {
 		var reader = new FileReader();
