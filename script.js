6ec94f9f7911a242d62e4c02e46e0d78cde0c397
Sahibjot Saggu <sahibjot@live.ca>
2016-04-05 16:45:39 -0400

    Test for showing main image on load

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-$(document).ready(function(){var t="@",e="gmail";$(".tile2_wrapper .projects_wrapper #projects_list div").css("height",$("#projects_list").width()*(9/16));var r=".",i="singh",s=$(".projects_wrapper .projects_list ul li").width();$(".projects_wrapper .projects_list ul li").css("height",s);var p=r;var o="sahibjot",c=document.getElementById("cf"),l="com",a="96";c.setAttribute("action","//formspree.io/"+i+p+o+a+t+e+r+l),$(window).resize(function(){var t=$(".projects_wrapper .projects_list ul li").width();$(".projects_wrapper .projects_list ul li").css("height",t),$(".tile2_wrapper .projects_wrapper #projects_list div").css("height",$("#projects_list").width()*(9/16))})});
+$(document).ready(function() {
+	$("#foreground_pic").on("load", setTimeout(function() {
+		console.log("hey");
+		$("#foreground_pic").css("display", "block");
+	}, 5000));
+
+	var t="@",e="gmail";
+	$(".tile2_wrapper .projects_wrapper #projects_list div").css("height",$("#projects_list").width()*(9/16));
+	var r=".",i="singh",s=$(".projects_wrapper .projects_list ul li").width();
+	$(".projects_wrapper .projects_list ul li").css("height",s);
+	var p=r;var o="sahibjot",c=document.getElementById("cf"),l="com",a="96";
+	c.setAttribute("action","//formspree.io/"+i+p+o+a+t+e+r+l),$(window).resize(function(){var t=$(".projects_wrapper .projects_list ul li").width();
+	$(".projects_wrapper .projects_list ul li").css("height",t),$(".tile2_wrapper .projects_wrapper #projects_list div").css("height",$("#projects_list").width()*(9/16))})});
