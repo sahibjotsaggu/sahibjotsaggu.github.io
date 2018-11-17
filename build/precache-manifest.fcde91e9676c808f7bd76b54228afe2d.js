e572ce9cdc8aa348f2a081ed4e023ef6e06e772d
Sahibjot Saggu <sahibjot@live.ca>
2018-11-16 23:56:43 -0800

    build files

---
+++ build/precache-manifest.fcde91e9676c808f7bd76b54228afe2d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa9e256460dafb44545e11f49823889a",
+    "url": "/static/media/uw-logo.fa9e2564.png"
+  },
+  {
+    "revision": "ce2a00e482f973999af0a4121903af4a",
+    "url": "/static/media/ubiq_icon_blue.ce2a00e4.svg"
+  },
+  {
+    "revision": "4cb1e35ee11e8ecc4bb5c37edaea498e",
+    "url": "/static/media/mozzaz-logo.4cb1e35e.png"
+  },
+  {
+    "revision": "3009f6f9a1b143c7e4674cae83689b95",
+    "url": "/static/media/klick-logo.3009f6f9.png"
+  },
+  {
+    "revision": "589ff659040fbf368aeea3a8f19fd01c",
+    "url": "/static/media/cf-logo-v.589ff659.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "668a5fed74ca0a098fe3",
+    "url": "/static/js/main.668a5fed.chunk.js"
+  },
+  {
+    "revision": "371811c6b87f94dbd722",
+    "url": "/static/js/1.371811c6.chunk.js"
+  },
+  {
+    "revision": "668a5fed74ca0a098fe3",
+    "url": "/static/css/main.f2f99afd.chunk.css"
+  },
+  {
+    "revision": "9bf227b5440e31156b00b3b75fdede10",
+    "url": "/index.html"
+  }
+];
